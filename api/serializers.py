c946808031839e67c826b464a046ddfcfb72de0d
No Body <igormikhilov850@gmail.com>
2022-04-12 15:59:02 +0800

    blin

--- api/serializers.py
+++ api/serializers.py
@@ -44,13 +44,7 @@ class ListingLocationSerializer(serializers.ModelSerializer):
 class ListingImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ListingImage
-        fields = ['id', 'image_url']
-
-    image_url = serializers.SerializerMethodField('get_image_url')
-
-    def get_image_url(self, obj):
-        request = self.context.get("request")
-        return request.build_absolute_uri(obj.image.url)
+        fields = ['id', 'image']
 
     def save(self, **kwargs):
         image = ListingImage.objects.create(**self.validated_data, listing_id=self.context['listing_id'])
