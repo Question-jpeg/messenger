41224ac2afc5396a3f3bdf7864c9a1398036c6fb
No Body <igormikhilov850@gmail.com>
2022-04-17 15:44:55 +0800

    adding amazon s3 service

--- DoneWithItBackend/settings/prod.py
+++ DoneWithItBackend/settings/prod.py
@@ -11,3 +11,10 @@ ALLOWED_HOSTS = ['krezer-donewithit.herokuapp.com']
 DATABASES = {
     'default': dj_database_url.config() #reads the DATABASE_URL environment variable, parses the connection and returns dictionary
 }
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.environ['BUCKETEER_AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['BUCKETEER_AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['BUCKETEER_BUCKET_NAME']
+AWS_QUERYSTRING_AUTH = False
