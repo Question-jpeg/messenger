027554a9d56646df2f9b62d7f0c074dba32ec14a
No Body <igormikhilov850@gmail.com>
2022-04-22 19:14:17 +0800

    new paths

--- api/models.py
+++ api/models.py
@@ -53,8 +53,8 @@ class ListingImage(models.Model):
         Listing, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to='api/images',
                               validators=[validate_file_size])
-    thumbnail_sm = ProcessedImageField(upload_to='api/images/thumbnails', format='JPEG',  options={'quality': 30}, null=True, blank=True)
-    thumbnail_lg = ProcessedImageField(upload_to='api/images/thumbnails', format='JPEG',  options={'quality': 50}, null=True, blank=True)
+    thumbnail_sm = ProcessedImageField(upload_to='api/images/thumbnails/small', format='JPEG',  options={'quality': 30}, null=True, blank=True)
+    thumbnail_lg = ProcessedImageField(upload_to='api/images/thumbnails/large', format='JPEG',  options={'quality': 50}, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         self.thumbnail_sm = self.image.file
