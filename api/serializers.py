9516ec46acef42d5feef24117e633d6e074148f6
No Body <igormikhilov850@gmail.com>
2022-05-25 23:19:42 +0800

    fix no avatar error

--- api/serializers.py
+++ api/serializers.py
@@ -16,8 +16,9 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 
         token['name'] = user.name
         token['email'] = user.email
-        token['avatar'] = user.avatar.url
-        token['avatar_thumbnail_sm'] = user.avatar_thumbnail_sm.url
+        if user.avatar:
+            token['avatar'] = user.avatar.url
+            token['avatar_thumbnail_sm'] = user.avatar_thumbnail_sm.url
 
         return token
 
