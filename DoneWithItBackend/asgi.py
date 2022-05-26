d693bf1cffe6bfc92b55c4c5a38f920d56d2076e
No Body <igormikhilov850@gmail.com>
2022-05-27 04:57:01 +0800

    weird test

--- DoneWithItBackend/asgi.py
+++ DoneWithItBackend/asgi.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from .routing import websocket_urlpatterns
